1537d79b70950b8fa0f9008dbe06cdcb6206ec72
Elias Roussos <eliasisaiah@outlook.com>
2019-07-29 20:51:43 -0400

    -grabbing input from user on search button and getting data from api -returning titles via query modification

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,50 @@
-$(document).ready(function(){
+$(document).ready(function () {
     const docArticles = $("#articles");
     const docInput = $("#searchTerm");
     const docStartYear = $("#startYear");
     const docEndYear = $("#endYear");
     const btnSearch = $("#search");
-    const btnClear  =   $("#clear");
+    const btnClear = $("#clear");
+    const docNumArticles = $("#numArticles");
     
+    let numArticles = 5;
+
     let input = "";
+    let url = "";
 
 
-    const apiKey= "A4S5DC01lXDXTlrU4Kb7hPJR2SnkFgME"
+    const apiKey = "A4S5DC01lXDXTlrU4Kb7hPJR2SnkFgME"
 
     btnSearch.on("click", (event) => {
+        
         event.preventDefault();
+
         input = docInput.val();
-        console.log(input);
+
+        //grab startYear and endYear input vals        
+        let startYear = docStartYear.val();
+        let endYear = docEndYear.val();
+
+        numArticles = docNumArticles.val();
+
+        url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${input}&api-key=${apiKey}`
+
+        $.get(url, function () {
+    
+        }).then(function (apiData) {
+            let articleArray = apiData.response.docs;
+            console.log(articleArray);
+            for( let i = 0; i < numArticles; i++) {
+
+                let articleElement = $("<div>");
+                let articleTitle = $("<h2>").addClass("title").text(articleArray[i].headline.main);
+                articleElement.append(articleTitle);
+                docArticles.append(articleElement);
+            };
+        });
+
     })
 
-    let url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=${apiKey}`
-    $.get(url, function(){
-        
-    }).then(function(apiData){
-        // console.log(apiData.response.docs);
-        // buildQuery(response);
-    });
 
     //TODO:
 
