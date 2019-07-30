6750499abe9129c0a448659f6e0497176f130049
Elias Roussos <eliasisaiah@outlook.com>
2019-07-29 20:27:16 -0400

    -implemented docInput send to value for query

--- assets/script.js
+++ assets/script.js
@@ -5,15 +5,33 @@ $(document).ready(function(){
     const docEndYear = $("#endYear");
     const btnSearch = $("#search");
     const btnClear  =   $("#clear");
+    
+    let input = "";
+
 
     const apiKey= "A4S5DC01lXDXTlrU4Kb7hPJR2SnkFgME"
+
+    btnSearch.on("click", (event) => {
+        event.preventDefault();
+        input = docInput.val();
+        console.log(input);
+    })
 
     let url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=${apiKey}`
     $.get(url, function(){
         
-    }).then(function(response){
-        console.log(response);
+    }).then(function(apiData){
+        // console.log(apiData.response.docs);
+        // buildQuery(response);
     });
+
+    //TODO:
+
+    //pull inputs from fields and insert into queryURL
+
+    //Click event handler for search button
+    //Click event handler for clear button
+
 
 
 
